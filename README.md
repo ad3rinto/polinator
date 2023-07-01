5f76ee2859dc1273b9edc877afe0c1da17dfd3c5
ad3rinto <aad3rinto@gmail.com>
2023-07-01 16:04:20 +0100

    defined new API end point. Update user interface to show pollen count. Also implemented colour coding, to reflect pollen levels.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 # polinator
 
 SImple web app that monitors pollen count over specific period of days utilizing weather data from
-this API: https://www.weatherbit.io/api/airquality-current
+this API: https://api.tomorrow.io
--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-//Example fetch using pokemonapi.co
+//Example fetch using https://api.tomorrow.io
+
+const dateToday = new Date().toDateString()
+document.querySelector('#dateOfDay').innerHTML = `<h1>${dateToday}</h1>`;
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+  const location = document.querySelector('#pCode').value
+
+  console.log(location)
+  const API_KEY = ''
+  const url = `https://api.tomorrow.io/v4/timelines?location=${location}&timesteps=1d&units=metric&apikey=${API_KEY}&fields=treeIndex,grassIndex,weedIndex`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      const pollenData = data.data.timelines[0].intervals[0].values
+      if (pollenData.treeIndex >= 3) {
+        document.querySelector("h3" + ".tree").innerHTML = "HIGH"
+        document.querySelector("h3" + ".tree").classList.add("high")
+      } else if (pollenData.treeIndex > 1) {
+        document.querySelector("h3" + ".tree").innerHTML = "MEDIUM"
+        document.querySelector("h3" + ".tree").classList.add("medium")
+      } else {
+        document.querySelector("h3" + ".tree").innerHTML = "LOW"
+        document.querySelector("h3" + ".tree").classList.add("low")
+      }
+      document.querySelector("h3" + ".grass").innerHTML = pollenData.grassIndex
+      document.querySelector("h3" + ".weed").innerHTML = pollenData.weedIndex
+
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
 }
