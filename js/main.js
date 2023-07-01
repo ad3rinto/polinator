90bbc2ed892804ae7f0e87b9cf5e40ac5e068ddc
ad3rinto <aad3rinto@gmail.com>
2023-07-01 16:24:28 +0100

    stored api key offline, and pulled it in via a js function

--- js/main.js
+++ js/main.js
@@ -4,11 +4,34 @@ const dateToday = new Date().toDateString()
 document.querySelector('#dateOfDay').innerHTML = `<h1>${dateToday}</h1>`;
 document.querySelector('button').addEventListener('click', getFetch)
 
+
+function importText(textFile) {
+  "use strict";
+  var rawFile = new XMLHttpRequest();
+  var allText = "";
+  rawFile.open("Get", textFile, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        allText = rawFile.responseText;
+      }
+    }
+  }
+  rawFile.send(null);
+  return allText;
+}
+
+const key = importText("key.txt")
+
+
+
+
+
 function getFetch() {
   const location = document.querySelector('#pCode').value
 
   console.log(location)
-  const API_KEY = ''
+  const API_KEY = key
   const url = `https://api.tomorrow.io/v4/timelines?location=${location}&timesteps=1d&units=metric&apikey=${API_KEY}&fields=treeIndex,grassIndex,weedIndex`
 
   fetch(url)
@@ -25,8 +48,31 @@ function getFetch() {
         document.querySelector("h3" + ".tree").innerHTML = "LOW"
         document.querySelector("h3" + ".tree").classList.add("low")
       }
-      document.querySelector("h3" + ".grass").innerHTML = pollenData.grassIndex
-      document.querySelector("h3" + ".weed").innerHTML = pollenData.weedIndex
+
+
+      if (pollenData.grassIndex >= 3) {
+        document.querySelector("h3" + ".grass").innerHTML = "HIGH"
+        document.querySelector("h3" + ".grass").classList.add("high")
+      } else if (pollenData.grassIndex > 1) {
+        document.querySelector("h3" + ".grass").innerHTML = "MEDIUM"
+        document.querySelector("h3" + ".grass").classList.add("medium")
+      } else {
+        document.querySelector("h3" + ".grass").innerHTML = "LOW"
+        document.querySelector("h3" + ".grass").classList.add("low")
+      }
+
+
+      if (pollenData.weedIndex >= 3) {
+        document.querySelector("h3" + ".weed").innerHTML = "HIGH"
+        document.querySelector("h3" + ".weed").classList.add("high")
+      } else if (pollenData.weedIndex > 1) {
+        document.querySelector("h3" + ".weed").innerHTML = "MEDIUM"
+        document.querySelector("h3" + ".weed").classList.add("medium")
+      } else {
+        document.querySelector("h3" + ".weed").innerHTML = "LOW"
+        document.querySelector("h3" + ".weed").classList.add("low")
+      }
+
 
     })
     .catch(err => {
